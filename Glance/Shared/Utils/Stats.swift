95f4a97f576483ac077d43fd1361381e996aef63
VictorPuga <39507381+VictorPuga@users.noreply.github.com>
2021-06-22 15:45:41 -0600

    Try to fix group container access

--- Glance/Shared/Utils/Stats.swift
+++ Glance/Shared/Utils/Stats.swift
@@ -7,11 +7,16 @@ class Stats {
 	private let dateCountsKey = "dateCount"
 	private let extensionCountsKey = "extensionCount"
 	private let totalCountKey = "totalCount"
-
+	
 	private let defaults: UserDefaults?
-
+	
 	init() {
-		defaults = UserDefaults(suiteName: "com.chamburr.glance")
+		defaults = UserDefaults(suiteName: "group.TZ5YSJH8XE.com.VictorPuga.Glance")
+		
+		createStatsFile()
+		// let i = defaults?.integer(forKey: "testCount") ?? 0
+		// defaults?.setValue(i + 1, forKey: "testCount")
+		
 		if defaults == nil {
 			os_log(
 				"Unable to initialize user defaults: Object is null",
@@ -54,5 +59,50 @@ class Stats {
 
 		// Increase total count by 1
 		defaults!.set(getTotalCount() + 1, forKey: totalCountKey)
+	}
+	
+	private func createStatsFile() {
+		let appIdentifier = "TZ5YSJH8XE.group.com.VictorPuga.Glance"
+		let fileManager = FileManager.default
+		guard let container = fileManager.containerURL(forSecurityApplicationGroupIdentifier: appIdentifier) else { return }
+		let directoryPath  = container.appendingPathComponent("Library/Application Support/Glance")
+		do{
+			var isDir: ObjCBool = false
+			let path = directoryPath.path
+			if fileManager.fileExists(atPath: path, isDirectory: &isDir)
+			{
+				if isDir.boolValue {
+					// file exists and is a directory
+				} else {
+					// file exists and is not a directory
+				}
+			} else {
+				// file or directory does not exist
+				try fileManager.createDirectory(at: directoryPath, withIntermediateDirectories: false, attributes: nil)
+			}
+			
+		} catch let error as NSError {
+			print(error.description)
+		}
+		// let statsFolder = directory
+		// 	.appendingPathComponent("Library/Application Support/Glance/TEST")
+		let stats = directoryPath.appendingPathComponent("stats.json")
+		var isDir: ObjCBool = false
+		if (fileManager.fileExists(atPath: stats.path, isDirectory: &isDir))
+		// if (try? Data.init(contentsOf: stats)) == nil
+		{
+			do {
+				try "{}".write(to:stats , atomically: true, encoding: .utf8)
+			} catch {
+				print(error)
+			}
+		} else {
+			do {
+				let i = Int.random(in: 0...100)
+				try "\(i)".write(to: stats, atomically: true, encoding: .utf8)
+			} catch {
+				print(error)
+			}
+		}
 	}
 }
